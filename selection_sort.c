569ea9878b0275beb931b8c847cdf08228cb4957
VARUN KUMAR <varunjha089@gmail.com>
2023-01-23 14:35:57 +0000

    Completed about selection sort

--- selection_sort.c
+++ selection_sort.c
@@ -1,4 +1,7 @@
 /*
+    Selection sort is a simple sorting algorithm that repeatedly selects the smallest element from an unsorted list 
+    and places it at the beginning of the sorted list. Here is an example implementation of selection sort in C:
+
     The function takes in an array arr and its size n as input. It iterates through the array, and in each iteration, 
     finds the index of the smallest element from the unsorted portion of the array. Then, it swaps the element at that 
     index with the first element of the unsorted portion, effectively moving the smallest element to the beginning of 
