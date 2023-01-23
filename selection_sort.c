6755f8309469557c733da11bfef88fe8b28e4d47
VARUN KUMAR <varunjha089@gmail.com>
2023-01-23 14:30:09 +0000

    Selection sort completed.

---
+++ selection_sort.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+void selection_sort(int arr[], int n);
+
+int main(){
+    int arr[7] = {6, 4, 8, 19, 18, 17, 11};
+    
+    selection_sort(arr, 7);
+    for(int i = 0; i<7; i++)
+        printf("%d ",arr[i]);
+    printf("\n");
+    return(0);
+}
+
+/*
+    The function takes in an array arr and its size n as input. It iterates through the array, and in each iteration, 
+    finds the index of the smallest element from the unsorted portion of the array. Then, it swaps the element at that 
+    index with the first element of the unsorted portion, effectively moving the smallest element to the beginning of 
+    the sorted portion of the array. This process is repeated until the entire array is sorted.
+    
+    It is important to keep in mind that selection sort is not an efficient sorting algorithm and has a time complexity 
+    of O(n^2) which makes it inefficient for large data sets.
+*/
+
+void selection_sort(int arr[], int n) {
+    int i, j, min_idx, temp;
+    for (i = 0; i < n-1; i++) {
+        min_idx = i;
+        for (j = i+1; j < n; j++) {
+            if (arr[j] < arr[min_idx]) {
+                min_idx = j;
+            }
+        }
+        temp = arr[min_idx];
+        arr[min_idx] = arr[i];
+        arr[i] = temp;
+    }
+}
