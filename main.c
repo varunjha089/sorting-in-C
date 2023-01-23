f8b646dbce6fb19b2c13a8b328de89e6f8ca36e3
VARUN KUMAR <varunjha089@gmail.com>
2023-01-23 14:49:22 +0000

    Added switch case to choose multiple sorting algo.

--- main.c
+++ main.c
@@ -1,11 +1,34 @@
 #include <stdio.h>
 #include "selection_sort.c"
+#include "bubble_sort.c"
 // void selection_sort(int arr[], int n);
 
 int main(){
     int arr[7] = {6, 4, 8, 19, 18, 17, 11};
+    int n;
     
-    selection_sort(arr, 7);
+    printf("Please select one sorting algo\n");
+    printf("1. Selection Sort\n");
+    printf("2. Bubble Sort\n");
+
+    scanf("%d", &n);
+
+    switch (n)
+    {
+    case 1:
+        printf("You have selected Selection Sort\n");
+        selection_sort(arr, 7);
+        break;
+    
+    case 2:
+        printf("You have selected Bubble Sort\n");
+        bubbleSort(arr, 7);
+        break;
+
+    default:
+        break;
+    }
+
     for(int i = 0; i<7; i++)
         printf("%d ",arr[i]);
     printf("\n");
